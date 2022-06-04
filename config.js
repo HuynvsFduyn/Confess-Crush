d42bf4326dd2353c0ab39569fa519255160c8cf9
HuynvsFduyn <106815814+HuynvsFduyn@users.noreply.github.com>
2022-06-04 08:20:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Đồng ý r đấy nhó.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/duynduyn04' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
